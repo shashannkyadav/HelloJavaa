c8aef46fe3b0ef0473c6666d6167c1f45c906f58
Shashank Yadav <yadavshashank377@gmail.com>
2023-07-08 21:31:20 +0530

    Day1Assignment

---
+++ AssignmentDay1.java
@@ -0,0 +1,132 @@
+package HelloJava;
+
+public class AssignmentDay1 {
+    public static void main(String[]args){
+        // 1: int store 4 bytes of data
+        int a = 1; int b = 2; int c = 3; int d = 4; int e = 5; int f = 6;
+        int g = 7; int h = 8; int i = 9; int j = 10;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+
+        // 2: Float store 4 bytes of the data
+        //it is storing 6 to 7 decimal value
+        float num1 = 5.99f; float num2 = 6.99f;float num3 = 3.99f; float num4 = 4.99f; float num5 = 7.99f;
+        float num6 = 2.99f; float num7 = 7.99f; float num8 = 8.99f; float num9 = 9.99f; float numb = 22.99f;
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(num7);
+        System.out.println(num8);
+        System.out.println(num9);
+        System.out.println(numb);
+
+        // 3: Char is storing 2 bytes of the data
+        // it is storing single character/letter or ASCII values
+
+        char letter = 'a'; char letter1 = 'b';  char letter2 = 'c';  char letter3 = 'd';  char letter4 = 'e';  char letter5 = 'f';
+        char letter6 = 'j';  char letter7 = 'i';  char letter8 = 'h';  char letter9 = 'g';
+
+        System.out.println(letter);
+        System.out.println(letter1);
+        System.out.println(letter2);
+        System.out.println(letter3);
+        System.out.println(letter4);
+        System.out.println(letter5);
+        System.out.println(letter6);
+        System.out.println(letter7);
+        System.out.println(letter8);
+        System.out.println(letter9);
+
+        // 4: Boolean always gives true and false
+
+        boolean myBool = true;
+        boolean helBool = false;
+
+        System.out.println(myBool);
+        System.out.println(helBool);
+
+        // 5: Byte stores 1 byte of data
+        // whole numbers from -128 to 128
+
+        byte myNum = 100; byte myNum1 = 120; byte myNum2 = 30; byte myNum3 = 40; byte myNum4 = 50; byte myNum5 = 60;
+        byte myNum6 = 80; byte myNum7 = 10; byte myNum8 = 90; byte myNum9 = 99;
+
+        System.out.println(myNum);
+        System.out.println(myNum1);
+        System.out.println(myNum2);
+        System.out.println(myNum3);
+        System.out.println(myNum4);
+        System.out.println(myNum5);
+        System.out.println(myNum6);
+        System.out.println(myNum7);
+        System.out.println(myNum8);
+        System.out.println(myNum9);
+
+        // 6: Short  stores 2 byte of data
+        // it is store small whole number integer
+
+        short myNu = 1000; short myNu1 = 20000; short myNu2 = 3000; short myNu3 = 4000; short myNu4 = 5000;
+        short myNu5 = 6000; short myNu6 = 7000;  short myNu7 = 8000; short myNu8 = 9000; short myNu9 = 1100;
+
+        System.out.println(myNu);
+        System.out.println(myNu1);
+        System.out.println(myNu2);
+        System.out.println(myNu3);
+        System.out.println(myNu9);
+        System.out.println(myNu4);
+        System.out.println(myNu5);
+        System.out.println(myNu6);
+        System.out.println(myNu7);
+        System.out.println(myNu8);
+
+        // 7: Long store 8 byte of data
+        // It stores very long whole number
+
+        long myLong = 15000000000L; long myLong1 = 12000000000L; long myLong2 = 11000000000L; long myLong3 = 14000000000L;
+        long myLong4 = 16000000000L; long myLong5 = 17000000000L; long myLong6 = 13000000000L; long myLong7 = 18000000000L;
+        long myLong8 = 19000000000L; long myLong9 = 10000000000L;
+
+        System.out.println(myLong);
+        System.out.println(myLong1);
+        System.out.println(myLong2);
+        System.out.println(myLong3);
+        System.out.println(myLong4);
+        System.out.println(myLong5);
+        System.out.println(myLong6);
+        System.out.println(myLong7);
+        System.out.println(myLong8);
+        System.out.println(myLong9);
+
+
+        // 8: Double stores 8 byte of data
+        // It  Stores very big decimal value
+
+        double myDouble = 19.999d;double myDouble1 = 191.999d;double myDouble2 = 14.999d;double myDouble3 = 18.98799d;double myDouble4 = 12.29949d;
+        double myDouble5 = 102.57764d;double myDouble6 = 1575.85789d;double myDouble7 = 155.95749d;double myDouble8 = 15.779d;double myDouble9 = 14.5599d;
+
+        System.out.println(myDouble);
+        System.out.println(myDouble1);
+        System.out.println(myDouble2);
+        System.out.println(myDouble3);
+        System.out.println(myDouble4);
+        System.out.println(myDouble5);
+        System.out.println(myDouble6);
+        System.out.println(myDouble7);
+        System.out.println(myDouble8);
+        System.out.println(myDouble9);
+
+
+    }
+}
